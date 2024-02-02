9036d66f940ec30dce6e0c82416d129e67f9ea2c
NocerGal <luc.schenherr@gmail.com>
2024-02-02 15:27:26 +0100

    ajout de content et image à getPostDatas

--- app/posts/view-post/[...post]/page.tsx
+++ app/posts/view-post/[...post]/page.tsx
@@ -13,33 +13,22 @@ export default async function postsPage({
 }: {
   params: { post: string };
 }) {
-  const getPostLikes = await prisma.post.findUnique({
+  const getPostDatas = await prisma.post.findUnique({
     where: {
       id: params.post[0],
     },
     select: {
       Like: true,
+      content: true,
+      image: true,
     },
   });
 
-  const markdown = `# A demo of "react-markdown"
+  if (!getPostDatas || getPostDatas === null) return;
 
-  "react-markdown" is a markdown component for React.
-  
-  👉 Changes are re-rendered as you type.
-  
-  👈 Try writing some markdown on the left.
-  
-  ## Overview
-  
-  * Follows [CommonMark](https://commonmark.org)
-  * Optionally follows [GitHub Flavored Markdown](https://github.github.com/gfm/)
-  * Renders actual React elements instead of using "dangerouslySetInnerHTML"
-  * Lets you define your own components.
-  * Has a lot of plugins
-  `;
+  const markdown = getPostDatas.content as string;
 
-  const userLikeBoolean = !getPostLikes?.Like.some(async (like) => {
+  const userLikeBoolean = !getPostDatas?.Like.some(async (like) => {
     // session dans le some, car sinon, ne permet pas à un utilisateur non connecté de voir le post
     const session = await getServerSession(authOptions);
     like.userId === session?.user.id;
@@ -52,15 +41,15 @@ export default async function postsPage({
 
   return (
     <div className="flex flex-col gap-12">
-      <Image
-        className="h-[48vh] object-cover object-center"
-        src={
-          'https://img.freepik.com/photos-gratuite/close-up-hommes-mains-taper-clavier-ordinateur-portable_1262-2250.jpg?w=1800&t=st=1703706230~exp=1703706830~hmac=25d28f71d7061601f564da101f8373cfb4b5249dd20a67eae446be1029b1eb6d'
-        }
-        alt="image représentant le post"
-        width={1000}
-        height={1}
-      />
+      {getPostDatas.image && (
+        <Image
+          className="h-[48vh] object-cover object-center"
+          src={getPostDatas.image as string}
+          alt="image représentant le post"
+          width={1000}
+          height={1}
+        />
+      )}
       <Markdown className={style.reactMarkDown}>{markdown}</Markdown>
 
       <div className="flex justify-end cursor-pointer">
@@ -69,9 +58,9 @@ export default async function postsPage({
           userLike={!userLikeBoolean}
           postId={params.post[0]}
           likeCount={
-            getPostLikes === null || getPostLikes === undefined
+            getPostDatas === null || getPostDatas === undefined
               ? 9999
-              : getPostLikes.Like.length
+              : getPostDatas.Like.length
           }
         />
       </div>
