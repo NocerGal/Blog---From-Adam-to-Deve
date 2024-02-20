33e7370cd5c4317fadb7cd4f93c7b8ca2bdedca3
NocerGal <luc.schenherr@hotmail.fr>
2024-02-20 07:39:42 +0100

    Correction d'un bug visuel - Ajout d'un toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import { TailwindIndicator } from '@/components/TailwindIndicator';
+// import { TailwindIndicator } from '@/components/TailwindIndicator';
 
 import { cn } from '@/lib/utils';
 
-import { Inter, Mina } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 import { Providers } from './Providers';
 import './globals.css';
 import Header from '@/components/Layout/Header';
 import Footer from '@/components/Layout/Footer';
-import Head from 'next/head';
+
 import { Metadata } from 'next';
 
 const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' });
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
+    <html lang="en" className="h-full bg-background" suppressHydrationWarning>
       <body
         className={cn(
           'h-full bg-background font-sans antialiased',
@@ -28,14 +28,16 @@ export default function RootLayout({ children }: PropsWithChildren) {
         )}
       >
         <Providers>
-          <div className="relative flex min-h-screen flex-col">
+          <div className="flex min-h-screen flex-col bg-background">
             <Header />
-            <main className="flex-1 px-4">
-              <div className="w-full max-w-4xl mx-auto pt-6">{children}</div>
+            <main className="flex-1 h-full px-4 bg-background border-solid border-red border-t-2">
+              <div className="flex-1 w-full max-w-4xl mx-auto pt-6 ">
+                {children}
+              </div>
             </main>
             <Footer />
           </div>
-          <TailwindIndicator />
+          {/* <TailwindIndicator /> */}
         </Providers>
       </body>
     </html>
